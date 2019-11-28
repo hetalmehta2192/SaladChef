efd858d01ac82cd691a64d3ca20681941f80577a
Hetal <hetalmehta2192@gmail.com>
2019-11-28 16:08:50 +0530

    Game elements and UI Setup

---
+++ SaladChefProj/Assets/Scripts/CameraMotion/CameraPanController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class CameraPanController : MonoBehaviour
+{
+	[SerializeField]
+	public float m_zoomSpeed = 1.5f;
+
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.position += Vector3.up * Time.deltaTime * Input.mouseScrollDelta.y;
+	}
+	//	public class CameraPan
+	//	{
+	//		public KeyCode m_zoomInKey;
+	//		public KeyCode m_zoomOutKey;
+	//	}
+}
